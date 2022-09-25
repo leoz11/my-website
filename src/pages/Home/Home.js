1934b1ee6be1ddcaae1a54ce437ea679c3fd946e
leonardo <106268245+leoz11@users.noreply.github.com>
2022-09-24 23:47:37 -0300

    Update files

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -26,7 +26,15 @@ const Home = () => {
     <div className='content'>
       <div>
         <h2>Conhecimentos</h2>
-        <p></p>
+        <p>
+          <img src="https://img.shields.io/badge/HTML5-E34F26?style=for-the-badge&logo=html5&logoColor=white" alt='html'/>
+          <img src="https://img.shields.io/badge/CSS3-1572B6?style=for-the-badge&logo=css3&logoColor=white" alt='css'/>
+          <img src="https://img.shields.io/badge/JavaScript-F7DF1E?style=for-the-badge&logo=javascript&logoColor=black" alt='javascript'/>
+          <img src="https://img.shields.io/badge/Node.js-43853D?style=for-the-badge&logo=node.js&logoColor=white" alt="node" />
+          <img src="https://img.shields.io/badge/React-20232A?style=for-the-badge&logo=react&logoColor=61DAFB" alt='react'/>
+          <img src="https://img.shields.io/badge/Bootstrap-563D7C?style=for-the-badge&logo=bootstrap&logoColor=white" alt="bootstrap" />
+
+        </p>
         <button>Projetos</button>
       </div>
     </div>
